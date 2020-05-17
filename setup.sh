d3c54b58544a478c52ad6c0e6d6999e927a88894
MαđЋανα <57581347+Madhava-mng@users.noreply.github.com>
2020-05-17 23:05:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/bash
-if [ -f /data/data/com.termux/files/usr/bin/ls ]
-then
-    cp zipet /data/data/com.termux/files/usr/bin/zipet && chmod +x /data/data/com.termux/files/usr/bin/zipet
-else
-    cp zipet /usr/bin/zipet && chmod +x /usr/bin/zipet
-fi
+if [ -f /data/data/com.termux/files/usr/bin/ls ]; then cp zipet /data/data/com.termux/files/usr/bin/zipet && chmod +x /data/data/com.termux/files/usr/bin/zipet; else cp zipet /usr/bin/zipet && chmod +x /usr/bin/zipet; fi
